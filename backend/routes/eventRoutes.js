dba78d1033ce60d8c8aaa5545478abaa30642303
anrix05 <anrix05@users.noreply.github.com>
2026-09-16 07:28:45 +0530

    chore: remove unused legacy backend folder for vercel deployment

--- backend/routes/eventRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const eventController = require('../controllers/eventController');
-
-router.get('/', eventController.getEvents);
-router.post('/', eventController.createEvent);
-router.post('/:id/rsvp', eventController.rsvpEvent);
-
-module.exports = router;
--- backend/routes/jobRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const jobController = require('../controllers/jobController');
-
-router.get('/', jobController.getJobs);
-router.post('/', jobController.createJob);
-router.post('/:id/apply', jobController.applyJob);
-
-module.exports = router;
